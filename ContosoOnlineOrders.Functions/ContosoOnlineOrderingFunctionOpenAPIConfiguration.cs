04e73ee337142c4d969e91560526b81e6959386f
brady gaster <bradyg@microsoft.com>
2021-02-21 14:56:38 -0800

    added functions openapi config

---
+++ ContosoOnlineOrders.Functions/ContosoOnlineOrderingFunctionOpenAPIConfiguration.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.Azure.WebJobs.Extensions.OpenApi.Core.Configurations;
+using Microsoft.OpenApi.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ContosoOnlineOrders.Functions
+{
+    public class ContosoOnlineOrderingFunctionOpenAPIConfiguration : IOpenApiConfigurationOptions
+    {
+        public OpenApiInfo Info { get; set; } = new OpenApiInfo()
+        {
+            Version = "1.0.0",
+            Title = "Contoso Online Ordering Functions",
+            Description = "Functions used by Contoso Online Ordering",
+            Contact = new OpenApiContact()
+            {
+                Name = "Contoso",
+                Email = "order-support@contoso.com"
+            }
+        };
+
+        public List<OpenApiServer> Servers
+        {
+            get
+            {
+                return (new OpenApiServer[] { new OpenApiServer { Url = 
+                    Environment.GetEnvironmentVariable("WEBSITE_HOSTNAME")
+                }}).ToList();
+            }
+            set => throw new NotImplementedException();
+        }
+    }
+}

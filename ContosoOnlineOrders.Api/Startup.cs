f1b69f4f878dad53a9f26b42460320b3a126f2fb
brady gaster <bradyg@microsoft.com>
2021-02-21 10:52:30 -0800

    Revert "added table provider"
    This reverts commit e691649b17ead6d1a337f102c1b1628467bf5aff.

--- ContosoOnlineOrders.Api/Startup.cs
+++ ContosoOnlineOrders.Api/Startup.cs
@@ -32,11 +32,8 @@ namespace ContosoOnlineOrders.Api
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            //services.AddMemoryCache();
-            //services.AddSingleton<IStoreDataService, MemoryCachedStoreServices>();
-            services.AddAzureTableStorageDataService(
-                Configuration.GetConnectionString("ContosoOnlineConnectionString")
-                );
+            services.AddMemoryCache();
+            services.AddSingleton<IStoreDataService, MemoryCachedStoreServices>();
             services.AddControllers();
             services.AddSwaggerGen(c =>
             {

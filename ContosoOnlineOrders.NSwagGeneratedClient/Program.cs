6d31a2bb267adaaf9b51e076f8e4fe63b02022bf
Jon Galloway <jongalloway@gmail.com>
2021-02-22 18:07:43 -0800

    Converted ConsoleClient to async

--- ContosoOnlineOrders.NSwagGeneratedClient/Program.cs
+++ ContosoOnlineOrders.NSwagGeneratedClient/Program.cs
@@ -1,13 +1,14 @@
-﻿using System;
+using System;
 using System.Net.Http;
+using System.Threading.Tasks;
 
 namespace ContosoOnlineOrders.ConsoleClient
 {
     class Program
     {
-        static void Main(string[] args)
+        static async Task Main(string[] args)
         {
-            using(var httpClient = new HttpClient())
+            using var httpClient = new HttpClient();
             {
                 var apiClient = new ContosoOnlineOrders_ApiClient(httpClient);
 
@@ -18,7 +19,7 @@ namespace ContosoOnlineOrders.ConsoleClient
                 ------------------------------------------------------------*/
 
                 // create a product
-                apiClient.CreateProductAsync("1.1", new CreateProductRequest
+                await apiClient.CreateProductAsync("1.1", new CreateProductRequest
                 {
                     Id = 1000,
                     InventoryCount = 0,
@@ -26,7 +27,7 @@ namespace ContosoOnlineOrders.ConsoleClient
                 });
 
                 // update a product's inventory
-                apiClient.UpdateProductInventoryAsync(1000, "1.1",
+                await apiClient.UpdateProductInventoryAsync(1000, "1.1",
                     new InventoryUpdateRequest
                     {
                         CountToAdd = 50,
@@ -34,34 +35,34 @@ namespace ContosoOnlineOrders.ConsoleClient
                     });
 
                 // get all products
-                apiClient.GetProductsAsync("1.1");
+                await apiClient.GetProductsAsync("1.1");
 
                 // get one product
-                apiClient.GetProductAsync(1000, "1.1");
+                await apiClient.GetProductAsync(1000, "1.1");
 
                 // create a new order
                 Guid orderId = Guid.NewGuid();
 
-                apiClient.CreateOrderAsync("1.1", new Order
+                await apiClient.CreateOrderAsync("1.1", new Order
                 {
                     Id = orderId,
                     Items = new CartItem[]
                     {
-                        new CartItem { ProductId = 1000, Quantity = 10 }
+            new CartItem { ProductId = 1000, Quantity = 10 }
                     }
                 });
 
                 // get one order
-                apiClient.GetOrderAsync(orderId, "1.1");
+                await apiClient.GetOrderAsync(orderId, "1.1");
 
                 // get all orders
-                apiClient.GetOrdersAsync("1.1");
+                await apiClient.GetOrdersAsync("1.1");
 
                 // check an order's inventory
-                apiClient.CheckInventoryAsync(orderId, "1.1");
+                await apiClient.CheckInventoryAsync(orderId, "1.1");
 
                 // ship an order
-                apiClient.ShipOrderAsync(orderId, "1.1");
+                await apiClient.ShipOrderAsync(orderId, "1.1");
 #else
                 /*------------------------------------------------------------
                 Without explicit operationId: Note how the generated SDK
@@ -72,7 +73,7 @@ namespace ContosoOnlineOrders.ConsoleClient
                 ------------------------------------------------------------*/
 
                 // create a product
-                apiClient.ProductsAllAsync(new CreateProductRequest
+                await apiClient.ProductsAllAsync(new CreateProductRequest
                 {
                     Id = 1000,
                     InventoryCount = 0,
@@ -80,7 +81,7 @@ namespace ContosoOnlineOrders.ConsoleClient
                 });
 
                 // update a product's inventory
-                apiClient.CheckInventory2Async(1000,                           
+                await apiClient.CheckInventory2Async(1000,                           
                 new InventoryUpdateRequest
                     {
                         CountToAdd = 50,
@@ -88,15 +89,15 @@ namespace ContosoOnlineOrders.ConsoleClient
                     });
 
                 // get all products
-                apiClient.ProductsAsync();
+                await apiClient.ProductsAsync();
 
                 // get one product
-                apiClient.Products2Async(1000);
+                await apiClient.Products2Async(1000);
 
                 // create a new order
                 Guid orderId = Guid.NewGuid();
 
-                apiClient.OrdersAsync(new Order
+                await apiClient.OrdersAsync(new Order
                 {
                     Id = orderId,
                     Items = new CartItem[]
@@ -106,16 +107,16 @@ namespace ContosoOnlineOrders.ConsoleClient
                 });
 
                 // get one order
-                apiClient.Orders2Async(orderId);
+                await apiClient.Orders2Async(orderId);
 
                 // get all orders
-                apiClient.OrdersAllAsync();
+                await apiClient.OrdersAllAsync();
 
                 // check an order's inventory
-                apiClient.CheckInventoryAsync(orderId);
+                await apiClient.CheckInventoryAsync(orderId);
 
                 // ship an order
-                apiClient.ShipAsync(orderId);
+                await apiClient.ShipAsync(orderId);
 #endif
             }
 
